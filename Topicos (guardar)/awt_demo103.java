b08966fa8ef2b50fb728e91a0b871a9e0ef698bd
shrimphellnah <glitchstercalls@gmail.com>
2025-07-07 12:19:13 -0600

    Cambiado el nombre de boton de ingles a español

--- Topicos (guardar)/awt_demo103.java
+++ Topicos (guardar)/awt_demo103.java
@@ -3,11 +3,11 @@ public class awt_demo103 extends Frame
 {
  public awt_demo103()
  {
-  super("Ejemplo de Frame");setSize(300,300);
+  super("Ejemplo de Frame");setSize(600,300);
   setLayout(new FlowLayout());
   Button boton = new Button("Aceptar"); add(boton);
   Button boton1 = new Button("Cancelar");add(boton1);
-  Button boton2 = new Button("push the red botton");add(boton2);
+  Button boton2 = new Button("pulsa el boton rojo");add(boton2);
   Button boton3 = new Button("Peligro no presionar");add(boton3);
   boton3.setBackground(Color.GREEN);
   setBackground(Color.yellow);
